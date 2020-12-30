208dc520520f2ff1708b765b858680569ebe3894
MOHD USMAN ALI <40804741+mohdusman63@users.noreply.github.com>
2020-12-30 18:10:35 +0530

    Create static_class.js

---
+++ static_class.js
@@ -0,0 +1,16 @@
+class Person{
+ 
+  static getName(){
+    console.log('usman azmi')
+  }
+  static pushName(){
+    console.log('abcd')
+  }
+  static add(a,b){
+    let z=a+b
+    console.log(z)
+  }
+}
+Person.getName()
+Person.pushName()
+Person.add(1,2)
